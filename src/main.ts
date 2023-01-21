57ec626f64bf6d4f53f16c622e608b17f4499fb7
Paolo Ráez <str237@gmail.com>
2023-01-21 20:57:23 +0100

    Move player

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,34 @@
 import kaboom from 'kaboom'
 
 kaboom()
+
+const player = add([
+  pos(0, 0),
+  circle(16),
+])
+
+const obstacle = add([
+  pos(50, 50),
+  circle(10),
+  color(rgb(255, 0, 0))
+])
+
+player.onUpdate(() => {
+  camPos(player.pos)
+})
+
+onKeyDown('a', () => {
+  player.pos.x -= 1
+})
+
+onKeyDown('w', () => {
+  player.pos.y -= 1
+})
+
+onKeyDown('s', () => {
+  player.pos.y += 1
+})
+
+onKeyDown('d', () => {
+  player.pos.x += 1
+})
